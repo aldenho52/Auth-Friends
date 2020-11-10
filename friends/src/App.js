14348514e77d5602e9fb6a60caafebe560ffc6c7
Alden Ho <aldenho@Aldens-MacBook-Air.local>
2020-11-10 14:29:07 -0700

    added friendlist and friend names populating

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,18 +1,17 @@
-import React, {useState} from 'react'
+import React from 'react'
 import './App.css';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import FriendList from './components/FriendList'
 import Login from './components/Login'
 import PrivateRoute from './components/PrivateRoute'
-
-import axiosWithAuth from './utils/axiosWithAuth'
 
 function App() {
 
   return (
     <Router>
       <div className="App">
+      
         <h1>Friends</h1>
         <Switch>
         <PrivateRoute exact path ='/protected' component={FriendList}/>
--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -1,7 +1,30 @@
+import React, {useState, useEffect} from 'react'
+import axiosWithAuth from '../utils/axiosWithAuth'
+
+
 const FriendList = () => {
+    const [friends, setFriends] = useState([])
+
+    const getData = () => {
+        axiosWithAuth()
+        .get('/friends')
+        .then(res => {
+            setFriends(res.data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
     return (
         <div>
-        
+        {friends.map(friend => {
+            return <p key={friend.id}>{friend.name}</p>
+        })}
         </div>
     )
 }
